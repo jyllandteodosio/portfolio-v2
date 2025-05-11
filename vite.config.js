760d01fdcc105752fd02f0e7e485c8be17629c0e
Jylland Gayle Teodosio Bienes <jylland.teodosio@gmail.com>
2025-05-11 17:43:03 +0200

    add fs

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from "vite-plugin-svgr";
+import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
