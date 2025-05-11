192f635b827649f5fe489a5d062e1aeed49de1be
Jylland Gayle Teodosio Bienes <jylland.teodosio@gmail.com>
2025-05-11 17:52:14 +0200

    remove server temp

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import svgr from "vite-plugin-svgr";
-import fs from 'fs';
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,11 +17,5 @@ export default defineConfig({
         assetFileNames: 'assets/[name]-[hash][extname]',
       },
     },
-  },
-  server: { // Add this server configuration block
-    https: {
-      key: fs.readFileSync('/etc/nginx/ssl/fullchain1.pem'),
-      cert: fs.readFileSync('/etc/nginx/ssl/privkey1.pem'),
-    }
   },
 })
